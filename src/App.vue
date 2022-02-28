b13d6f5aafb196498951f1bfe60f53a2620b988e
Rodolfo Vasquez <smis037117@ugb.edu.sv>
2022-02-28 05:19:30 -0600

    Pequeños arreglos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-
+    <b-button v-b-toggle.sidebar-right class="grey darken-4">INTEGRANTES</b-button>
+    <b-sidebar id="sidebar-right" title="Parcial #1" right shadow >
+      <div class="px-3 py-2">
+        <h4>INTEGRANTES</h4>
+        <p>Mauricio Enmanuel Montiel Ruiz - SMIS952220</p>
+        <p>Jose Rodolfo Vasquez Bonilla - SMIS037117</p>
+        <br><br>
+        <p>
+        NOTA: 
+        Este parcial solo se hizo entre 2 personas.
+        </p>
+      </div>
+    </b-sidebar>
     <mostrar-datos/>
   
   </div>
--- src/components/MostrarDatos.vue
+++ src/components/MostrarDatos.vue
@@ -38,7 +38,7 @@
         <br>
         
         <div class="row">
-          <button type="submit" class="btn purple darken-4">Agregar usuario <b-icon icon="archive-fill"></b-icon></button>
+          <button type="submit" class="btn purple darken-4">{{(indice == -1 ? 'Agregar':'actualizar')}} <b-icon icon="archive-fill"></b-icon></button>
         </div>
       </form> 
     </div>
@@ -50,19 +50,21 @@
         <thead>
           <tr>
             <th>Nombre <b-icon icon="file-person"></b-icon></th>
-            <th>Apellido <b-icon icon="file-person"></b-icon></th><br>
-            <th>DUI <b-icon icon="card-heading"></b-icon></th><br>
+            <th>Apellido <b-icon icon="file-person"></b-icon></th>
+            <th>DUI <b-icon icon="card-heading"></b-icon></th>
             <th>Modelo de Carro <b-icon icon="truck"></b-icon></th>
             <th>Precio <b-icon icon="cart"></b-icon></th>
           </tr>
         </thead>
         <tbody>
-            <tr v-for="usuario in usuarios" v-bind:key="usuario">
+            <tr v-for="(usuario,index) in usuarios" v-bind:key="usuario">
               <td>{{usuario.nombre}}</td>
-              <td>{{usuario.apellido}}</td><br>
-              <td>{{usuario.DUI}}</td><br>
+              <td>{{usuario.apellido}}</td>
+              <td>{{usuario.DUI}}</td>
               <td>{{usuario.modelodecarro}}</td>
               <td>{{usuario.precio}}</td>
+              <td><a class="grey darken-4" @click="editar (index)"><b-icon icon="pencil-square"></b-icon></a></td>
+              <td><a class= "grey darken-4" @click="eliminar (index)"><b-icon icon="trash-fill"></b-icon></a></td>
             </tr>
         </tbody>
       </table>
@@ -81,9 +83,9 @@ export default {
     name: "MostrarDatos",
     data(){
     return {
+      indice: -1,
       nombre: '',
       apellido: '',
-      edad: 0,
       DUI: '',
       modelodecarro: '',
       precio: 0,
@@ -103,19 +105,48 @@ export default {
       var data = {
         nombre: this.nombre,
         apellido: this.apellido,
-        edad: this.edad,
         modelodecarro: this.modelodecarro,
         DUI: this.DUI,
         precio: this.precio
       };
-      this.usuarios.push(data);
+      if(this.indice == -1)
+      {
+        this.usuarios.push(data);
+      }
+      else
+      {
+        this.usuarios[this.indice] = data;
+      }
+      this.indice = -1;
       this.nombre = '';
       this.apellido = '';
-      this.DUI = 0;
+      this.DUI = '';
       this.modelodecarro = '';
       this.precio = 0;
     },
+   eliminar(index)
+    {
+      if(!confirm('¿Desea eliminar este registro?'))return;
+
+      this.usuarios.splice(index, 1);
+
+    },
+    editar(index)
+    {
+      var usuario = this.usuarios[index];
+      this.indice = index;
+      this.nombre = usuario.nombre;
+      this.apellido = usuario.apellido;
+      this.DUI = usuario.DUI;
+      this.modelodecarro = usuario.modelodecarro;
+      this.precio = usuario.precio;
+
+
+
+
+    }
   }
+   
 
 }
 
